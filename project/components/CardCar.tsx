86a45435a0f5779c8e45204645100e423096d667
Clemente Nogueira <git.3gear@gmail.com>
2023-06-19 23:23:35 +0100

    The CardDetails component was created and styled.

--- project/components/CardCar.tsx
+++ project/components/CardCar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import {calculateCarRent} from "@/utils";
+import {calculateCarRent, generateCarImageUrl} from "@/utils";
 import Image from "next/image";
 import {CustomButton} from "@/components/CustomButton";
 import {useState} from "react";
@@ -49,7 +49,7 @@ export const CardCar = ({car}: CardCarProps) => {
                     </span>
                 </p>
                 <div className="relative w-full h-40 my-3 object-contain">
-                    <Image src="/hero.png" alt="hero" fill priority className="object-contain"/>
+                    <Image src={generateCarImageUrl(car)} alt='car model' fill priority className="object-contain"/>
                 </div>
                 <div className="relative flex w-full mt-2">
                     <div className='flex group-hover:invisible w-full justify-between text-grey'>
--- project/utils/index.ts
+++ project/utils/index.ts
@@ -1,3 +1,5 @@
+import {CarProps} from "@/components/CardCar";
+
 export async function fetchCars() {
     const url = process.env.URL_API as string;
     const options = {
@@ -32,3 +34,18 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
 
     return rentalRatePerDay.toFixed(0);
 };
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+    const url = new URL("https://cdn.imagin.studio/getimage");
+    const { make, model, year } = car;
+
+    url.searchParams.append('customer', process.env.NEXT_PUBLIC_IMAGIN_API_KEY || '');
+    url.searchParams.append('make', make);
+    url.searchParams.append('modelFamily', model.split(" ")[0]);
+    url.searchParams.append('zoomType', 'fullscreen');
+    url.searchParams.append('modelYear', `${year}`);
+    // url.searchParams.append('zoomLevel', zoomLevel);
+    url.searchParams.append('angle', `${angle}`);
+
+    return `${url}`;
+}
